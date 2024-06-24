1222df1370d1843105a3dd42a07b9698bd35dbba
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-06-24 10:46:12 -0300

    snackBar

--- parte_2/lib/pages/snackbar/snackbar_page.dart.dart
+++ parte_2/lib/pages/snackbar/snackbar_page.dart.dart
@@ -7,10 +7,37 @@ class CustomSnackBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("SnackBar Cutomizada"),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [],
+      appBar: AppBar(
+        title: const Text("SnackBar Cutomizada"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Snackbar clicada com sucesso"),
+                  ),
+                );
+              },
+              child: const Text("Simple SnackBar"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    duration: const Duration(seconds: 2),
+                    action: SnackBarAction(label: "X", onPressed: () {}),
+                    content: const Text("Snackbar clicada com sucesso"),
+                  ),
+                );
+              },
+              child: const Text("Action SnackBar"),
+            ),
+          ],
+        ),
       ),
     );
   }
