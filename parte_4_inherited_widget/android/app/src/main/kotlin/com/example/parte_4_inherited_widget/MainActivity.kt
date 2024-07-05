2d982087ef47673591970e32202b810f3958d618
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-07-05 09:23:27 -0300

    fix on the part 4

---
+++ parte_4_inherited_widget/android/app/src/main/kotlin/com/example/parte_4_inherited_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.parte_4_inherited_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ parte_4_inherited_widget/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RootApp());
+}
+
+
+class RootApp extends StatelessWidget {
+  const RootApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "InheritedWidgets",
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+
+    );
+  }
+}
