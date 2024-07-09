5be24d5f58ca323b03ce9890f5dfb9c303e434da
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-07-09 09:41:07 -0300

    finalizzando módulo de sqlite

--- parte_5_sqlite/lib/database/data_base.dart
+++ parte_5_sqlite/lib/database/data_base.dart
@@ -4,34 +4,60 @@ import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DataBaseSqlite {
-  Future<void> openConnection() async {
+  Future<Database> openConnection() async {
     String databaseLocalPath = await getDatabasesPath();
-    final String databaseFullPath = join(databaseLocalPath, "SQLITE_EXAMPLE");
+    final String databaseFullPath = join(databaseLocalPath, "bancodedados.db");
 
-    await openDatabase(
+    return await openDatabase(
       databaseFullPath,
-      version: 1,
-      onCreate: (Database db, version) {
-        print("On Create");
-        final batch = db.batch();
-        batch.execute(
-          """
-      CREATE TABLE teste(
-      id integer primary key autoincrement,
-      nome varchar(60),
-      idade int,
-      )
-      """,
+      version: 2,
+      onConfigure: (db) async {
+        print('OnConfigure');
+
+        await db.execute("PRAGMA foreing_keys==ON");
+      },
+      onCreate: (Database db, int version) async {
+        print("OnCreate");
+        await db.execute(
+          "CREATE TABLE teste(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,age INTEGER)",
         );
+
+        await db.transaction(
+          (action) async {
+            await action.rawInsert("INSERT INTO teste VALUES(0,'Breno',22)");
+            await action.rawInsert("INSERT INTO teste VALUES(1,'Lucas',23)");
+          },
+        );
+
+        Batch batch = db.batch();
+        batch.insert("teste", {"name": 'Amanda', 'age': 18});
+        await batch.commit();
       },
-      onUpgrade: (db, oldVersion, newVersion) {
-        print("On Updgrade");
+      onUpgrade: (Database db, int oldVersion, int newVersion) {
+        print("OnUpgrade");
+        Batch batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute(
+            "CREATE TABLE Produtos(id INTEGER PRIMARY KEY,name TEXT,value INTEGER)",
+          );
+          batch.commit();
+        }
+
+        if (oldVersion == 2) {
+          batch.execute(
+              "CREATE TABLE Categoria(id INTEGER PRIMARY KEY AUTOINCREMENT,nome VARCHAR(80))");
+        }
       },
-      onDowngrade: (db, oldVersion, newVersion) {
-        print("On DownGrade");
+      onDowngrade: (Database db, int oldVersion, int newVersion) {
+        print("OnDownGrade");
+        Batch batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute("DROP TABLE Categoria");
+          batch.commit();
+        }
       },
     );
-
-    return;
   }
 }
--- parte_5_sqlite/lib/home_page.dart
+++ parte_5_sqlite/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parte_5_sqlite/database/data_base.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +13,39 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    DataBaseSqlite().openConnection();
+    _openDatabase();
+  }
+
+  Future<Database> _openDatabase() async {
+    Database database = await DataBaseSqlite().openConnection();
+    // await database.insert('teste', {"name": "Edilson", "age": 55});
+    // await database.delete('teste', where: 'name = ?', whereArgs: ["Raysa"]);
+    // await database.update(
+    //   "teste",
+    //   {"name": "Batman", "age": 40},
+    //   where: "name = ?",
+    //   whereArgs: ["Conceição"],
+    // );
+
+    // var databasequery = await database.query('teste');
+    // database.rawInsert(
+    //     "INSERT INTO teste(name,age) VALUES('Roberto Caros', 32)", []);
+
+    // database.delete('teste', where: "name = ?", whereArgs: ["Edilson"]);
+
+    // print(
+    //   databasequery.map(
+    //     (element) => print("${element.keys}:${element.values}"),
+    //   ),
+    // );
+
+    // database.delete("teste", where: "age = ?", whereArgs: [40]);
+
+    database.rawUpdate(
+      'UPDATE teste SET name = ? where age = ?',["Lionel Messi",78],
+    );
+
+    return database;
   }
 
   @override
